6140fc27e36c1bdc4544abf34c35ad5ea1ca7a72
Robin Beck <robin.a.beck@gmail.com>
2017-09-13 10:37:39 -0400

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'haproxy'
+
+allwebservers = search('node', 'recipe:apache\:\:default')
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+  variables ({
+    webservers => allwebservers
+  })
+  notifies :restart, 'service[haproxy]'
+end
+
+service 'haproxy' do
+  action [:start, :enable]
+end
